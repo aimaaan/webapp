4a9a84d127fba18526e4ba57435e1dba0960e05d
aimaaan <99475237+aimaaan@users.noreply.github.com>
2023-06-19 12:10:34 +0800

    js / css update for proper displaying

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-    $('#search').on('keyup', function() {
-        var location = $(this).val();
-
-        // Simulate fetching data based on location
-        var properties = [
+    var properties = [
             { location: 'Putrajaya', description: 'Bazzar Ramadhan Masjid Besi',  imageUrl: 'https://assets.hmetro.com.my/images/articles/2016_PJ060616FH004_1585627422.jpg' },
             { location: 'Kuala Lumpur', description: 'Bazzar Ramadhan Kuala Lumpur', imageUrl: 'https://live.staticflickr.com/7111/7652632638_59433eb21b_b.jpg' },
             { location: 'Johor', description: 'Bazzar Ramadhan Johor', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
@@ -11,29 +7,29 @@ $(document).ready(function() {
             { location: 'Negeri Sembilan', description: 'Bazzar Ramadhan Negeri Sembilan', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
             { location: 'Selangor', description: 'Bazzar Ramadhan Danau kota', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
             { location: 'Kelantan', description: 'Bazzar Ramadhan Kelantan', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
-            { location: 'Selangor', description: 'Bazzar Ramadhan Bangi', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
-            { location: 'Pahang', description: 'Bazzar Ramadhan Pahang', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
-            { location: 'Perak', description: 'Bazzar Ramadhan Perak', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
-            { location: 'Kedah', description: 'Bazzar Ramadhan Kedah', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
-            { location: 'Penang', description: 'Bazzar Ramadhan Penang', imageUrl: 'https://www.johorkini.my/wp-content/uploads/bfi_thumb/bazar-ramadhan-p4egiucvgqace6prxplp24n689lf3bxhb7qrebte68.jpg' },
-        ];
+    ];
 
-        var filteredProperties = properties.filter(function(property) {
-            return property.location.toLowerCase().indexOf(location.toLowerCase()) > -1;
-        });
+    $('#search').on('keyup', function() {
+        var location = $(this).val();
 
-        $('#properties').empty();
+        if (location !== '') {
+            var filteredProperties = properties.filter(function(property) {
+                return property.location.toLowerCase().indexOf(location.toLowerCase()) > -1;
+            });
 
-        filteredProperties.forEach(function(property) {
-            var propertyCard = `
-                <div class="property-card">
-                    <img src="${property.imageUrl}" alt="${property.description}">
-                    <h2>${property.location}</h2>
-                    <p>${property.description}</p>
-                </div>
-            `;
+            $('#properties').empty();
 
-            $('#properties').append(propertyCard);
-        });
+            filteredProperties.forEach(function(property) {
+                var propertyCard = `
+                    <div class="property-card">
+                        <img src="${property.imageUrl}" alt="${property.description}">
+                        <h2>${property.location}</h2>
+                        <p>${property.description}</p>
+                    </div>
+                `;
+
+                $('#properties').append(propertyCard);
+            });
+        }
     });
 });
